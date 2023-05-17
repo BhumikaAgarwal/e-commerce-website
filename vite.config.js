a353e9e2dd7eaf3923023d07f2d3ac7ee56e66ff
Bhumika Agarwal <119135387+BhumikaAgarwal@users.noreply.github.com>
2023-05-17 17:32:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/e-commerce-website",
 
   plugins: [react()],
 })
